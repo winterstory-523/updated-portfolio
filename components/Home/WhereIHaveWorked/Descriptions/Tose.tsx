f898326ae94db46d145ab16991339d806f47bd40
winterstory-523 <richard.stewart0523@gmail.com>
2023-12-10 00:24:50 +0800

    where ive worked update - 4
    Co-authored-by: Erielle Bancolita <eriellebancolita017@gmail.com>
    Co-authored-by: Louis Walter <developer.louiswalter@gmail.com>
    Co-authored-by: Fernando Mondragon Maytorena <developer.fernandomonmay@gmail.com>
    Co-authored-by: Amazing Dev <amazingdev0815@gmail.com>

--- components/Home/WhereIHaveWorked/Descriptions/Tose.tsx
+++ components/Home/WhereIHaveWorked/Descriptions/Tose.tsx
@@ -2,23 +2,19 @@ import React from "react";
 import ArrowIcon from "../../../Icons/ArrowIcon";
 import { getTasksTextWithHighlightedKeyword } from "./taskAndType";
 
-export default function Sertus() {
+export default function Tose() {
   const tasks = [
     {
-      text: "Delivered new features and fixing bugs with advanced technologies (React, Typescript, styled-components).",
-      keywords: ["new features", "fixing bugs"],
+      text: "Built new Admin website of game application by MERN stack (MongoDB, Express.js, React.js, Node.js), and maintained for over 6 months.",
+      keywords: ["Admin website", "MERN"],
     },
     {
-      text: "By writing unit tests for UI components, main functionalities, boosted the test coverage by over 90%",
-      keywords: ["unit tests", "over 90%"],
+      text: "Collaborated with teammates to improve the website by refactoring, optimizing and implementing unit-test and end-to-end tests (Jest, Mocha, Cypress), resulting in a 15% decrease in average page load time",
+      keywords: ["Collaborated", "refactoring, optimizing", "unit-test and end-to-end tests"],
     },
     {
-      text: "Collaborated with cross-functioning team of 5 top-tier engineers to find out best working solutions with latest libraries in React ecosystem.",
-      keywords: ["Collaborated"],
-    },
-    {
-      text: "Implemented new additional features from customers while working directly with CEO, CTO",
-      keywords: ["working directly"],
+      text: "Worked full-time for over 40 hours a week for approximately 18 months, communicated directly with clients and successfully delivered results",
+      keywords: ["full-time"],
     },
   ];
   return (
@@ -27,17 +23,17 @@ export default function Sertus() {
         <div className="flex flex-col spacey-y-2">
           {/* Title */}
           <span className="text-gray-100 sm:text-lg text-sm font-Arimo tracking-wide">
-            UI/UX Developer <span className="text-AAsecondary">@ React</span>
+            Full-Stack Developer <span className="text-AAsecondary">@ MERN</span>
           </span>
           {/* Date */}
-          <span className="font-mono text-xs text-gray-500">February 2019 - February 2020</span>
+          <span className="font-mono text-xs text-gray-500">June 2020 - December 2021</span>
           <span
             className="font-mono text-xs text-AAsecondary hover:cursor-pointer"
             // set text size on style attribute
             style={{ fontSize: "0.6rem" }}
-            onClick={() => window.open("https://sertus.uk/", "_blank")}
+            onClick={() => window.open("https://www.tose.com.ph/", "_blank")}
           >
-            sertus.uk
+            www.tose.com.ph
         </span>
         </div>
         <div className="flex flex-col space-y-4 sm:text-sm text-xs">
