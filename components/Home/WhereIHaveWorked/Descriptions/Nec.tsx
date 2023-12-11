ec2df0563d109c4decdaf54e857c86d6790ce2e7
winterstory-523 <richard.stewart0523@gmail.com>
2023-12-11 10:05:40 +0800

    where ive worked update - 5
    Co-authored-by: Erielle Bancolita <eriellebancolita017@gmail.com>
    Co-authored-by: Louis Walter <developer.louiswalter@gmail.com>
    Co-authored-by: Fernando Mondragon Maytorena <developer.fernandomonmay@gmail.com>
    Co-authored-by: Amazing Dev <amazingdev0815@gmail.com>

--- components/Home/WhereIHaveWorked/Descriptions/Nec.tsx
+++ components/Home/WhereIHaveWorked/Descriptions/Nec.tsx
@@ -2,23 +2,19 @@ import React from "react";
 import ArrowIcon from "../../../Icons/ArrowIcon";
 import { getTasksTextWithHighlightedKeyword } from "./taskAndType";
 
-export default function Signature() {
+export default function Nec() {
   const tasks = [
     {
-      text: "Contributed to official website frontend using Vue.js/Nuxt.js",
-      keywords: ["Vue.js/Nuxt.js"],
+      text: "Worked with senior front-end developers and learned front-end principles as a part-time role by 20 hrs per week",
+      keywords: ["Worked", "learned"],
     },
     {
-      text: "Delivered adding new features, building new pages, improving existing features & UI, maintaining the entire website",
-      keywords: ["adding", "improving", "maintaining"],
+      text: "Practically used React, Next.js, Vue.js and Nuxt.js in 2 large projects and 7 small projects and contributed the advance of the company",
+      keywords: ["2 large projects", "7 small projects"],
     },
     {
-      text: "Optimized Frontend performance and speed by writing high-quality code of best practices, tested it on page speed insights, etc",
-      keywords: ["high-quality", "best practices"],
-    },
-    {
-      text: "Communicated with backend team to integrate backend APIs and services",
-      keywords: ["integrate", "APIs", "services"],
+      text: "Used team working tools such as version control system (Git, Bitbucket), agile systems (JIRA, Trello, Slack) etc",
+      keywords: ["version control system", "agile systems"],
     },
   ];
   return (
@@ -27,17 +23,17 @@ export default function Signature() {
         {/* Title */}
         <span className="text-gray-100 sm:text-lg text-sm font-Arimo tracking-wide">
           
-          Frontend Development <span className="text-AAsecondary">@ Nuxt.js</span>
+          Junior Front-End Developer <span className="text-AAsecondary">@ React, Vue.js, Angular</span>
         </span>
         {/* Date */}
-        <span className="font-mono text-xs text-gray-500">June 2018 - November 2018</span>
+        <span className="font-mono text-xs text-gray-500">August 2018 - February 2020</span>
         <span
             className="font-mono text-xs text-AAsecondary hover:cursor-pointer"
             // set text size on style attribute
             style={{ fontSize: "0.6rem" }}
-            onClick={() => window.open("https://www.signature.co.nz/", "_blank")}
+            onClick={() => window.open("http://www.ntsp.com.ph/", "_blank")}
           >
-            www.signature.co.nz
+            www.ntsp.com.ph
         </span>
       </div>
       <div className="flex flex-col space-y-4 sm:text-sm text-xs">
